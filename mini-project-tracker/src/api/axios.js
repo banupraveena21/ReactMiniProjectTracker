0e9119d2227d685581e392d2c60e4fba453fd98b
Banu Praveena21 <banupraveena21@gmail.com>
2025-09-16 20:07:03 +0530

    axios.js

--- mini-project-tracker/src/api/axios.js
+++ mini-project-tracker/src/api/axios.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://reactminiprojecttracker.onrender.com";
 
 const instance = axios.create({
   baseURL: API_BASE_URL,
